e81a93e2e0fbadc3d23e8929bba56c2592a5d868
dollcg24 <dollygogri@gmail.com>
2020-03-16 00:15:20 +0530

    endpoints models

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', # add django rest framework
+    # apps
+    'apps.endpoints',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'diabrisk',
+        'USER':'Dolly',
+        'PASSWORD':'Dollygogri@24',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
